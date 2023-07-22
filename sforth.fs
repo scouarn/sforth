bb6e70857b631d4f190a27abcb67eba121f5d024
scouarn <admin@scouarn.fr>
2023-07-22 17:16:08 +0200

    Adjusted string literals

--- sforth.fs
+++ sforth.fs
@@ -4,6 +4,14 @@ HEAD ; ] C3 C, HIDE 0 STATE ! [ C3 C, HIDE IMMEDIATE
 : \ 0A PARSE DROP DROP ; IMMEDIATE \ Now we can comment
 : ( 29 PARSE DROP DROP ; IMMEDIATE ( Now we have both types of comments )
 
+\ TODO
+\ Primitives / Arithmetic / Comparisons
+\ For loops
+\ CASE
+\ DEFER
+\ CONSTANT / VARIABLE / CREATE / DOES> ...
+\ Numeric output
+
 
 : RECURSE HIDE ; IMMEDIATE
 
@@ -42,13 +50,6 @@ HEAD ; ] C3 C, HIDE 0 STATE ! [ C3 C, HIDE IMMEDIATE
 : REPEAT ( C: ori dest --      ) (   -- ) branch  resolve> <resolve ; IMMEDIATE
 
 
-
-: / ( x1 x2 -- x3 ) /MOD NIP ;
-: DEPTH ( -- +n ) SP@ S0 SWAP - 8 / ; \ TODO: use 2/ 2/ 2/ instead
-: ? ( addr -- ) @ . ;
-: SEE ( "<spaces>ccc<space>" -- ) PARSE-NAME FIND . . ;
-: .S [ HERE . ] ( x * i -- ) BEGIN DEPTH 0> WHILE CR . REPEAT ;
-
 : ' ( "<spaces>name" -- xt ) PARSE-NAME FIND NIP ;
 
 : POSTPONE ( "<spaces>name" -- )
@@ -69,33 +70,28 @@ HEAD ; ] C3 C, HIDE 0 STATE ! [ C3 C, HIDE IMMEDIATE
 : CHAR   ( "<spaces>name" -- char )           PARSE-NAME DROP C@ ;
 : [CHAR] ( C: "<spaces>name" -- ) ( -- char ) CHAR POSTPONE LITERAL ; IMMEDIATE
 
-
-
 : SLITERAL ( C: c-addr1 u ) ( -- c-addr2 u )
-    DUP >R
+    >R
     POSTPONE AHEAD          \ Skip string data
-    R> HERE >R ALLOT        \ Allocate string
+    R@ HERE >R ALLOT        \ Allocate string and save it's addr
     POSTPONE THEN
-    R@  POSTPONE LITERAL    \ Push c-addr2
-    DUP POSTPONE LITERAL    \ Push u
-    ( c-addr1 u ) R> SWAP ( c-addr1 c-addr2 u ) MOVE \ Copy string data
-; IMMEDIATE
-
-: C" ( C: "ccc<quote>" -- ) ( -- c-addr ) \ Counted string !
-    [CHAR] " PARSE
-    DUP >R
-    POSTPONE AHEAD          \ Skip string data
-    R> HERE >R
-    DUP C,                  \ Store length
-    ALLOT                   \ Allocate string
-    POSTPONE THEN
-    R@ POSTPONE LITERAL     \ Push c-addr2
-    R> CHAR+ SWAP ( c-addr1 c-addr3 u ) MOVE \ Copy string data
+    R@ POSTPONE LITERAL R>  \ Push c-addr2
+    R@ POSTPONE LITERAL R>  \ Push u
+    ( c-addr1 c-addr2 u ) MOVE \ Copy string data
 ; IMMEDIATE
 
 : S" ( C: "ccc<quote>" -- ) ( -- c-addr u ) [CHAR] " PARSE POSTPONE SLITERAL ; IMMEDIATE
-: ." ( C: "ccc<quote>" -- ) ( -- )          POSTPONE S" POSTPONE TYPE        ; IMMEDIATE
-: .( (    "ccc<quote>" -- ) ( -- )          [CHAR] ) PARSE TYPE ;
+: ." ( C: "ccc<quote>" -- ) ( --          ) POSTPONE S" POSTPONE TYPE        ; IMMEDIATE
+: .( (    "ccc<quote>" -- ) ( --          ) [CHAR] ) PARSE TYPE ;
+: C" ( C: "ccc<quote>" -- ) ( -- c-addr   )
+    [CHAR] " PARSE >R
+    POSTPONE AHEAD          \ Skip string data
+    R@ HERE >R              \ Keep addr of counted string
+    DUP C, ALLOT            \ Store length and allocate chars
+    POSTPONE THEN
+    R@ POSTPONE LITERAL     \ Push c-addr2
+    R> CHAR+ R> ( c-addr1 c-addr3 u ) MOVE \ Copy string data
+; IMMEDIATE
 
 : ABORT" ( C: "ccc<quote>" -- ) ( i*x x -- | i*x ) ( R: j*x -- | j*x )
     POSTPONE IF
@@ -106,7 +102,12 @@ HEAD ; ] C3 C, HIDE 0 STATE ! [ C3 C, HIDE IMMEDIATE
     POSTPONE THEN
 ; IMMEDIATE
 
-: HELLO CR ." Hello, World!" CR ;
-HELLO
 
-1 2 3 .S
+: / ( x1 x2 -- x3 ) /MOD NIP ;
+: DEPTH ( -- +n ) SP@ S0 SWAP - 8 / ; \ TODO: use 2/ 2/ 2/ instead
+: ? ( addr -- ) @ . ;
+: SEE ( "<spaces>ccc<space>" -- ) PARSE-NAME FIND . . ;
+: .S ( x * i -- ) DEPTH IF BEGIN DEPTH 0> WHILE CR . REPEAT ELSE ." EMPTY " THEN ;
+
+: HELLO ." Hello, World!" CR ;
+HELLO
