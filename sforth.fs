e7b88f5fbebcdfc38f407b089cf14b2cf4884724
scouarn <admin@scouarn.fr>
2023-07-24 15:45:08 +0200

    DO loops prototypes

--- sforth.fs
+++ sforth.fs
@@ -11,6 +11,9 @@
 \ Numeric input >NUMBER and standard number parser
 \ ENVIRONMENT?
 
+
+\ Utilities ====================================================================
+
 : / ( x1 x2 -- x3 ) /MOD NIP ;
 : CELL+ 8 + ;
 : CELLS 8 * ;
@@ -23,6 +26,7 @@
 : COUNT ( c-addr1 -- c-addr2 u ) DUP CHAR+ SWAP C@ ;
 
 
+\ Control flow =================================================================
 
 : <resolve ( C: ori      --     ) (   -- ) >R HERE 4 - R@ - R> H! ;
 : resolve> ( C: dest ori --     ) (   -- ) >R HERE - R> H! ;
@@ -45,6 +49,20 @@
 : WHILE  ( C: dest -- ori dest ) ( x -- ) branch0 SWAP              ; IMMEDIATE
 : REPEAT ( C: ori dest --      ) (   -- ) branch  resolve> <resolve ; IMMEDIATE
 
+\ TODO
+: DO?    ( C:    -- do ) ( n1 n2 -- ) ( R:       --  loop  ) ; IMMEDIATE
+: DO     ( C:    -- do ) ( n1 n2 -- ) ( R:       --  loop  ) ; IMMEDIATE
+: LOOP   ( C: do --    ) (       -- ) ( R: loop1 -- |loop2 ) ; IMMEDIATE
+: LOOP+  ( C: do --    ) (     n -- ) ( R: loop1 -- |loop2 ) ; IMMEDIATE
+
+: LEAVE   ( -- ) ( R: loop -- ) ;
+: UNLOOP  ( -- ) ( R: loop -- ) ;
+
+: I (   -- n|u ) ( R:       loop  -- loop        ) R@ ;
+: J (   -- n|u ) ( R: loop1 loop2 -- loop1 loop2 ) R> R@ SWAP >R ;
+
+
+\ POSTPONE =====================================================================
 
 : ' ( "<spaces>name" -- xt ) PARSE-NAME FIND NIP ;
 
@@ -63,6 +81,9 @@
 : ['] ( C: "<spaces>name" -- ) ( -- xt ) ' POSTPONE LITERAL ; IMMEDIATE
 : [COMPILE] ( C: "<spaces>name" -- ) ' COMPILE, ; IMMEDIATE
 
+
+\ Strings ======================================================================
+
 : CHAR   ( "<spaces>name" -- char )           PARSE-NAME DROP C@ ;
 : [CHAR] ( C: "<spaces>name" -- ) ( -- char ) CHAR POSTPONE LITERAL ; IMMEDIATE
 
@@ -76,9 +97,14 @@
     ( c-addr1 c-addr2 u ) MOVE \ Copy string data
 ; IMMEDIATE
 
-: S" ( C: "ccc<quote>" -- ) ( -- c-addr u ) [CHAR] " PARSE POSTPONE SLITERAL ; IMMEDIATE
-: ." ( C: "ccc<quote>" -- ) ( --          ) POSTPONE S" POSTPONE TYPE        ; IMMEDIATE
+: S" ( C: "ccc<quote>" -- ) ( -- c-addr u )
+    [CHAR] " PARSE POSTPONE SLITERAL ; IMMEDIATE
+
+: ." ( C: "ccc<quote>" -- ) ( --          )
+    POSTPONE S" POSTPONE TYPE ; IMMEDIATE
+
 : .( (    "ccc<quote>" -- ) ( --          ) [CHAR] ) PARSE TYPE ;
+
 : C" ( C: "ccc<quote>" -- ) ( -- c-addr   )
     [CHAR] " PARSE >R
     POSTPONE AHEAD          \ Skip string data
@@ -99,24 +125,27 @@
 ; IMMEDIATE
 
 
-: docreate ( -- a-addr ) R> 1+ ; \ 1+ to skip the ret
-    \ NOTE: The ret is never executed and we return to child's caller
-
-: CREATE   ( "<spaces>name" -- ) ( -- a-addr )
-    \ parent: <parent-code> <ret>
-    \ docreate: Push caller's PFA <ret>
-    \ <child-header> <call docreate> <data>  (call doesn't return)
-    : ['] docreate COMPILE, POSTPONE ;
-;
+\ CREATE DOES> =================================================================
 
 : >BODY ( xt -- a-addr ) 6 + ;
+
 : >code ( nt -- xt     ) 9 + ( len-addr ) DUP C@ ( len-addr len ) + 1+ ;
+
+: docreate ( -- a-addr ) R> 1+ ; \ 1+ to skip the ret
+    \ NOTE: The ret is never executed and we return to child's caller
 
 : dodoes ( -- ) ( R: ret -- )
     R> \ Start of child-code, popping the return address will exit from parent
     CP @ >R
     LATEST @ >code CP ! COMPILE, \ Redirect child's call
     R> CP !
+;
+
+: CREATE   ( "<spaces>name" -- ) ( -- a-addr )
+    \ parent: <parent-code> <ret>
+    \ docreate: Push caller's PFA <ret>
+    \ <child-header> <call docreate> <data>  (call doesn't return)
+    : ['] docreate COMPILE, POSTPONE ;
 ;
 
 : DOES> ( -- ) ( R: ret -- )
@@ -126,28 +155,45 @@
     \                   |
     \ <child-header> <call child-code> <data> (call doesn't return)
     POSTPONE dodoes
-    \ Start of child-code
-    POSTPONE R> POSTPONE 1+ \ Compile code to get child's PFA (inlined DOCREATE)
+    \ child-code starts here
+    POSTPONE R> POSTPONE 1+ \ Compile code to get child's PFA
+; IMMEDIATE
+
+: CONSTANT ( x "<spaces>name" -- ) ( -- x      ) : POSTPONE LITERAL POSTPONE ; ;
+: VARIABLE (   "<spaces>name" -- ) ( -- a-addr ) CREATE 0 , ;
+
+
+\ DEFER ========================================================================
+
+: EXECUTE ( i*x xt -- j*x ) [
+    4C C, 89 C, C0 C,       \ mov   %r8, %rax
+    4C C, 8B C, 45 C, 00 C, \ mov   (%rbp), %r8
+    48 C, 8D C, 6D C, 08 C, \ lea   8(%rbp), %rbp
+    FF C, D0 C,             \ call  *%rax
+] ;
+
+: DEFER  (    "<spaces>name" --     ) CREATE 0 , DOES> @ EXECUTE ;
+: DEFER@ (            xt1    -- xt2 ) >BODY @ ;
+: DEFER! (        xt2 xt1    --     ) >BODY ! ;
+
+: IS
+    STATE @ IF
+        POSTPONE ['] POSTPONE DEFER!
+    ELSE
+        ' DEFER!
+    THEN
+; IMMEDIATE
+
+: ACTION-OF
+    STATE @ IF
+        POSTPONE ['] POSTPONE DEFER@
+    ELSE
+        ' DEFER@
+    THEN
 ; IMMEDIATE
 
 
-\ : CONSTANT ( x "<spaces>name" -- ) ( -- x ) CREATE , DOES> @ ;
-: CONSTANT ( x "<spaces>name" -- ) ( -- x ) : POSTPONE LITERAL POSTPONE ; ;
-
-: VARIABLE (   "<spaces>name" -- ) ( -- a-addr ) CREATE 0 , ;
-\ : VARIABLE (   "<spaces>name" -- ) ( -- a-addr ) : HERE 13 + POSTPONE LITERAL POSTPONE ; 0 , ;
-
-0 1 - CONSTANT TRUE
-00 CONSTANT FALSE
-20 CONSTANT BL
-: SPACE BL EMIT ;
-
-VARIABLE BASE
-: HEX     ( -- ) 10 BASE ! ;
-: DECIMAL ( -- ) 0A BASE ! ;
-DECIMAL
-
-
+\ Tools ========================================================================
 
 : ? ( addr -- ) @ . ;
 : NAME>STRING ( nt -- c-addr u ) 9 + DUP 1+ SWAP C@ ;
@@ -169,14 +215,25 @@ DECIMAL
 ;
 
 
+\ Consts and vars  =============================================================
 
-: EXECUTE ( i*x xt -- j*x ) [
-    4C C, 89 C, C0 C,       \ mov   %r8, %rax
-    4C C, 8B C, 45 C, 00 C, \ mov   (%rbp), %r8
-    48 C, 8D C, 6D C, 08 C, \ lea   8(%rbp), %rbp
-    FF C, D0 C,             \ call  *%rax
-] ;
+0 1 - CONSTANT TRUE
+00 CONSTANT FALSE
+20 CONSTANT BL
+: SPACE BL EMIT ;
 
-: DEFER ( "<spaces>name" -- ) CREATE 0 , DOES> ( i*x -- j*x ) @ EXECUTE ;
+
+\ Number IO ====================================================================
+
+VARIABLE BASE
+: HEX     ( -- ) 10 BASE ! ;
+: DECIMAL ( -- ) 0A BASE ! ;
+DECIMAL
+
+
+\ ==============================================================================
+
+
+\ Interpreter ==================================================================
 
 
